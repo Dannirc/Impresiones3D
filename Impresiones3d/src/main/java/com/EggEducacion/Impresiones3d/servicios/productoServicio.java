af995f9f719c07b2009b062f46e7ae4c1b0c4e5c
Dannirc <37722439+Dannirc@users.noreply.github.com>
2021-11-02 19:08:13 -0300

    subo servicios

--- Impresiones3d/src/main/java/com/EggEducacion/Impresiones3d/servicios/productoServicio.java
+++ Impresiones3d/src/main/java/com/EggEducacion/Impresiones3d/servicios/productoServicio.java
@@ -1,25 +1,60 @@
 package com.EggEducacion.Impresiones3d.servicios;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.EggEducacion.Impresiones3d.entidades.Archivo;
 import com.EggEducacion.Impresiones3d.entidades.Categoria;
 import com.EggEducacion.Impresiones3d.entidades.Producto;
 import com.EggEducacion.Impresiones3d.repositorios.productoRepositorio;
 
+
 @Service
 public class productoServicio {
 	@Autowired
 	private productoRepositorio productoRepositorio;
 	
+	@Autowired
+	private ArchivoServicio archivoServicio;
+	
 	@Transactional
-	public Producto crearProducto(String id,Archivo archivo, Categoria categoria) throws Exception {
+	public Producto crearProducto(MultipartFile file,String id, Categoria categoria) throws Exception {
 		Producto producto = new Producto();
 		producto.setId(id);
-		producto.setNombre(archivo);
+		producto.setCategoria(categoria);
+		Archivo archivo = archivoServicio.guardar(file);
+		producto.setArchivo(archivo);
 		return productoRepositorio.save(producto);
+	}
+	
+	@Transactional
+	public void modificarProducto(MultipartFile file,String id, Categoria categoria) throws Exception {
+		Optional<Producto> respuesta = productoRepositorio.findById(id);
+		if (respuesta.isPresent()) {
+			Producto producto = respuesta.get();
+			producto.setId(id);
+			producto.setCategoria(categoria);
+			
+			String idArchivo=null;
+			if(producto.getArchivo() != null) {
+				idArchivo=producto.getArchivo().getId();
+			}
+			Archivo archivo = archivoServicio.actualizar(idArchivo,file);
+			producto.setArchivo(archivo);
+			productoRepositorio.save(producto);
+		} else {
+			throw new Exception("error en el modificar-producto");
+		}
+	}
+	
+	@Transactional
+	public List<Producto> listarTodos() {
+		return productoRepositorio.findAll();
 	}
 	
 }
