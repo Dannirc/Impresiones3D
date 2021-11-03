6e6bf951fc50d03667afd750721b4a95b167cb5c
Dannirc <37722439+Dannirc@users.noreply.github.com>
2021-11-03 00:46:41 -0300

    subo plantillas

---
+++ Impresiones3d/src/main/java/com/EggEducacion/Impresiones3d/controladores/IndexControlador.java
@@ -0,0 +1,21 @@
+package com.EggEducacion.Impresiones3d.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"/", "/index"})
+public class IndexControlador {
+	
+	@GetMapping
+	public String index() {		
+		return ("index");
+	}
+	
+	@GetMapping("/login")
+	public String login() {		
+		return ("login");
+	}
+	
+}
